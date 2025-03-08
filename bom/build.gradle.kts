0560b1491a7c3badfc489cbe457692f361a6fc20
Nils Krüger <radsteve@radsteve.net>
2025-03-08 13:37:12 +0100

    feat: delegating tasks, bom publishing

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     `java-platform`
+    `maven-publish`
+}
+
+javaPlatform {
+    allowDependencies()
 }
 
 dependencies {
@@ -7,5 +12,25 @@ dependencies {
         rootProject.childProjects
             .filter { (name) -> name != "bom" }
             .forEach { (_, project) -> api(project) }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["javaPlatform"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "rad-public"
+            url = uri("https://maven.radsteve.net/public")
+
+            credentials {
+                username = System.getenv("RAD_MAVEN_USER")
+                password = System.getenv("RAD_MAVEN_TOKEN")
+            }
+        }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
-tasks.register("buildAll") {
-    childProjects.forEach { (_, project ) ->
-        dependsOn(project.tasks.getByName("build"))
+private fun delegatingTask(name: String, delegateTask: String) {
+    tasks.register(name) {
+        childProjects.forEach { (_, project ) ->
+            runCatching {
+                dependsOn(project.tasks.getByName(delegateTask))
+            }
+        }
     }
 }
+
+delegatingTask("buildAll", "build")
+delegatingTask("publishAll", "publish")
