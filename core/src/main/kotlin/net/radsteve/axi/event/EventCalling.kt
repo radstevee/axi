4338d24122cf099a7412659c29939d274e534950
Nils Krüger <radsteve@radsteve.net>
2025-04-02 16:35:56 +0200

    perf: use awaitAll on deferreds for callEvent

--- core/src/main/kotlin/net/radsteve/axi/event/EventCalling.kt
+++ core/src/main/kotlin/net/radsteve/axi/event/EventCalling.kt
@@ -1,12 +1,19 @@
 package net.radsteve.axi.event
 
 import com.github.shynixn.mccoroutine.bukkit.callSuspendingEvent
-import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.future.asCompletableFuture
+import kotlinx.coroutines.future.asDeferred
 import net.radsteve.axi.plugin.AxiPluginHolder
 import org.bukkit.Bukkit
 import org.bukkit.event.Event
 
 /** Calls the given [event]. */
 public suspend fun callEvent(event: Event) {
-  Bukkit.getPluginManager().callSuspendingEvent(event, AxiPluginHolder.plugin()).joinAll()
+  // Not doing joinAll here because awaitAll launches
+  // them concurrently
+  Bukkit.getPluginManager()
+    .callSuspendingEvent(event, AxiPluginHolder.plugin())
+    .map { job -> job.asCompletableFuture().asDeferred() }
+    .awaitAll()
 }
