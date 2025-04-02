a592aa15749ea86b16c2145b7daff9b6c2fee78b
Nils Krüger <radsteve@radsteve.net>
2025-04-02 16:18:25 +0200

    fix: make SkinRegistry and GameTypeRegistry open

--- core/src/main/kotlin/net/radsteve/axi/npc/SkinRegistry.kt
+++ core/src/main/kotlin/net/radsteve/axi/npc/SkinRegistry.kt
@@ -4,7 +4,7 @@ import net.radsteve.axi.DelegatingRegistry
 import net.radsteve.axi.Registry
 
 /** A skin data registry, delegating to [AxiSkins]. */
-public class SkinRegistry : DelegatingRegistry<String, SkinData>(AxiSkins) {
+public open class SkinRegistry : DelegatingRegistry<String, SkinData>(AxiSkins) {
   /** Delegating skin data registry. */
   public companion object AxiSkins : Registry<String, SkinData>()
 }
--- game/src/main/kotlin/net/radsteve/axi/game/type/GameTypeRegistry.kt
+++ game/src/main/kotlin/net/radsteve/axi/game/type/GameTypeRegistry.kt
@@ -5,7 +5,7 @@ import net.radsteve.axi.DelegatingRegistry
 import net.radsteve.axi.Registry
 
 /** A game type registry, delegating to [AxiGameTypes]. */
-public class GameTypeRegistry : DelegatingRegistry<Key, GameType<*>>(AxiGameTypes) {
+public open class GameTypeRegistry : DelegatingRegistry<Key, GameType<*>>(AxiGameTypes) {
   /** Delegating game type registry. */
   public object AxiGameTypes : Registry<Key, GameType<*>>(modifiable = true)
 }
