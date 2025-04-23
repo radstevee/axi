75c88263bfa16b8dfdae90d1020bde4e3c49ec2e
Nils Krüger <radsteve@radsteve.net>
2025-04-23 20:22:42 +0200

    feat(gradle-plugin): improve dependency ext

--- gradle-plugin/src/main/kotlin/net/radsteve/axi/gradle/ext/AxiDependenciesExtension.kt
+++ gradle-plugin/src/main/kotlin/net/radsteve/axi/gradle/ext/AxiDependenciesExtension.kt
@@ -2,6 +2,9 @@ package net.radsteve.axi.gradle.ext
 
 import net.radsteve.axi.gradle.ext.AxiExtension.Companion.axi
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ExternalModuleDependency
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle
+import org.gradle.api.provider.Provider
 import javax.inject.Inject
 
 public abstract class AxiDependenciesExtension @Inject constructor(
@@ -10,8 +13,21 @@ public abstract class AxiDependenciesExtension @Inject constructor(
   /** Adds the given [dependencyNotation] as a runtime and compile-time library. */
   public fun runtime(dependencyNotation: Any) {
     val dep = project.dependencies.create(dependencyNotation)
-    project.dependencies.add("implementation", dep)
     project.dependencies.add("axiRuntime", dep)
+    project.dependencies.add("compileOnly", dep)
+  }
+
+  /** Adds the given [provider] as a runtime and compile-time library. */
+  public fun runtimeProvider(provider: Provider<out ExternalModuleDependency>) {
+    project.dependencies.addProvider("axiRuntime", provider)
+    project.dependencies.addProvider("compileOnly", provider)
+  }
+
+  /** Adds the given [bundleProvider] as a runtime and compile-time library. */
+  @JvmName("runtimeBundle")
+  public fun runtimeProvider(bundleProvider: Provider<ExternalModuleDependencyBundle>) {
+    project.dependencies.addProvider("axiRuntime", bundleProvider)
+    project.dependencies.addProvider("compileOnly", bundleProvider)
   }
 
   /** Adds the given axi [modules]. */
